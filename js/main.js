0f6a184cadbf06bcd9cee4a99dace85b023872fd
Fortesa <Fortesa>
2017-01-11 23:02:56 +0100

    updating

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon glyphicon-menu-up').addClass('glyphicon glyphicon-menu-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon glyphicon-menu-down').addClass('glyphicon glyphicon-menu-up');
+	}
+})
